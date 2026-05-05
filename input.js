6b631d181db18aed824c39a8ff41d8981d259bdf
WitcherxSN <shravan.naik30@gmail.com>
2026-05-06 01:30:03 +0530

    Project Last Section Added

--- input.js
+++ input.js
@@ -1,13 +1,28 @@
 const API_KEY = "5c6eecacaa291702f76b6593139c6199";
-
+ 
+  
 async function getWeather() {
+
+  
   let city = document.getElementById("searchInput").value;
+
+  
 
   let response = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
   );
 
   let data = await response.json();
+
+  if (data.cod !== 200) {
+  document.getElementById("errorMessage").innerText = "Please enter a valid city name.";
+  document.getElementById("errorMessage").classList.remove("hidden");
+  return;
+}
+
+document.getElementById("errorMessage").classList.add("hidden");
+
+
 
   document.getElementById("city").innerText = data.name + ", " + data.sys.country;
   document.getElementById("temp").innerText = data.main.temp + "°C";
@@ -16,7 +31,15 @@ async function getWeather() {
     document.getElementById("wind").innerText = " " + data.wind.speed + " m/s";
     document.getElementById("description").innerText = "" + data.weather[0].description;
     document.getElementById("feelslike").innerText = "Feels like: " + data.main.feels_like + "°C";
-    document.getElementById("date").innerText = new Date().toLocaleDateString();
+    document.getElementById("date").innerText = new Date(data.dt * 1000)
+  .toLocaleDateString("en-US", {
+    weekday: "long",
+    month: "long",
+    day: "numeric",
+    year: "numeric"
+  });
+
+
 
 if (data.weather[0].main === "Clear") {
   document.getElementById("weatherIcon").src = "sun.png";
@@ -27,6 +50,10 @@ if (data.weather[0].main === "Clear") {
 }
 
  document.getElementById("visibility").innerText = " " + data.visibility / 1000 + " km";
+
+
+
+ 
 
 
 
@@ -44,30 +71,38 @@ if (data.weather[0].main === "Clear") {
   console.log(forecastData);
 
   displayForecast(forecastData);
+
+  showForecast();
 }
 
 function displayForecast(data) {
   for (let i = 1; i <= 5; i++) {
     let dayData = data.list[(i - 1) * 8];
 
-    document.getElementById("day" + i).innerText = "day:" +
-      dayData.dt_txt.split(" ")[0];
+    document.getElementById("day" + i).innerText =
+  new Date(dayData.dt_txt).toLocaleDateString("en-US", {
+    weekday: "short",
+    month: "long",
+    day: "numeric"
+  });
 
-    document.getElementById("forecastDesc" + i).innerText = "forecast:" +
-      dayData.weather[0].description;
+    document.getElementById("forecastDesc" + i).innerText = "" +
+      dayData.weather[0].description.charAt(0).toUpperCase() + dayData.weather[0].description.slice(1);
 
-    document.getElementById("forecastTemp" + i).innerText = "Temp: " +
+    document.getElementById("forecastTemp" + i).innerText = " " +
       dayData.main.temp + "°C";
 
-    document.getElementById("forecastwind" + i).innerText =
-      "Wind: " + dayData.wind.speed + " m/s";
+    document.getElementById("forecastwind" + i).innerHTML =
+  `<img src="wind1.png" class="w-5 h-5 inline-block mr-1">
+   ${dayData.wind.speed} m/s`;
 
-    document.getElementById("humidity" + i).innerText =
-      "Humidity: " + dayData.main.humidity + "%";
+    document.getElementById("humidity" + i).innerHTML =
+  `<img src="humidity1.png" class="w-5 h-5 inline-block mr-1">
+   ${dayData.main.humidity}%`;
 
     if (dayData.weather[0].main === "Clear") {
       document.getElementById("weatherIcon" + i).src = "sun.png" ;
-      document.getElementById("weatherIcon" + i).className = "h-20 w-20 p-2  group-hover:scale-110 transition-transform duration-300 ";
+      document.getElementById("weatherIcon" + i).className = "h-25 w-25 p-2  group-hover:scale-110 transition-transform duration-300 ";
 
       
     } else {
@@ -75,4 +110,45 @@ function displayForecast(data) {
         `https://openweathermap.org/img/wn/${dayData.weather[0].icon}@2x.png`;
     }
   }
+}
+
+
+function changetofahrenheit() {
+  document.getElementById("fahrenheitButton").classList.add("bg-blue-600");
+  document.getElementById("fahrenheitButton").classList.remove("bg-gray-400");
+
+  document.getElementById("celsiusButton").classList.add("bg-gray-400");
+  document.getElementById("celsiusButton").classList.remove("bg-blue-600");
+ let tempElement = document.getElementById("temp");
+
+  if (tempElement.innerText.includes("°F")) return;
+
+  let tempValue = parseFloat(tempElement.innerText);
+  let newTemp = (tempValue * 9) / 5 + 32;
+
+  tempElement.innerText = newTemp.toFixed(1) + "°F";
+ 
+}
+
+function changetocelsius() {
+  document.getElementById("celsiusButton").classList.add("bg-blue-600");
+  document.getElementById("celsiusButton").classList.remove("bg-gray-400");
+
+  document.getElementById("fahrenheitButton").classList.add("bg-gray-400");
+  document.getElementById("fahrenheitButton").classList.remove("bg-blue-600");
+let tempElement = document.getElementById("temp");
+
+  if (tempElement.innerText.includes("°C")) return;
+
+  let tempValue = parseFloat(tempElement.innerText);
+  let newTemp = ((tempValue - 32) * 5) / 9;
+
+  tempElement.innerText = newTemp.toFixed(1) + "°C";
+  
+}
+
+function showForecast() {
+  document.getElementById("forecast").style.display = "flex";
+  document.getElementById("placeholderBox").style.display = "none";
+
 }
